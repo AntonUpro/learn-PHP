61aec77825dcce4acc6f6848b8b9b2f5f168f636
Anton Ulyanov <anton.ulyanow@yandex.ru>
2022-07-24 15:57:10 +0500

    v3

---
+++ test2-4.php
@@ -0,0 +1,10 @@
+<?php 
+/* Вот 
+еще проверяю
+комантарии*/
+$first_name = 'Anton'; 
+$last_name = 'Ulyanov';
+$full_name= $first_name." ".$last_name;
+$len=strlen($full_name);
+print "You full name $full_name. The numbers of letters in your name $len"
+?>
